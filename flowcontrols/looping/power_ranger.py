8d288b64e827df3a337006d498c6ebbc8da9415b
Deepakvm18 <deepakvm2@gmail.com>
2021-02-01 11:31:41 +0530

    adding flowcontrol

---
+++ flowcontrols/looping/power_ranger.py
@@ -0,0 +1,9 @@
+l=int(input("ENTER YOUR number  "))
+l1=l
+s=l
+i=l
+while(i>=2):
+    l=(l*10)+l1
+    s=s+l
+    i=i-1
+print(s)
---
+++ flowcontrols/looping/upto_limit_evennumber.py
@@ -0,0 +1,5 @@
+l=int(input("ENTER YOUR LIMIT  "))
+i=2
+while(i<=l):
+    print(i)
+    i=i+2
