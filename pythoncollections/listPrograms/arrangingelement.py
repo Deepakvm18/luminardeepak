30c414d1b9163b8d4beedde78f7d0e0fb9908a88
Deepakvm18 <deepakvm2@gmail.com>
2021-02-08 12:26:19 +0530

    list

--- pythoncollections/listPrograms/arrangingelement.py
+++ pythoncollections/listPrograms/arrangingelement.py
@@ -1,5 +1,17 @@
 lst=[10,11,10,12,13,10,9]
-ele=int(input("enter the eleemt you want to isolate"))
-for i in lst:
-    if(ele==i):
-
+ele=int(input("enter the element you want to isolate"))
+# olist=[]
+# for i in lst:
+#     if(ele==i):
+#         olist.append(i)
+#         lst.remove(i)
+# for i in olist:
+#     lst.append(i)
+# print(lst)
+for j in lst:
+    for i in range(0,len(lst)-1):
+         if(lst[i]==ele):
+            t=lst[i]
+            lst[i]=lst[i+1]
+            lst[i+1]=t
+print(lst)
--- pythoncollections/listPrograms/binarySearch.py
+++ pythoncollections/listPrograms/binarySearch.py
@@ -2,10 +2,11 @@ arr=[10,12,12,13,19,18,29,20]
 element=int(input("ENTER THE ELEMENT YOU WANT TO SEARCH"))
 arr.sort()
 low=0
+pos=0
 flag=0
 upp=len(arr)-1
 
-while(low<upp):
+while(low<=upp):
     mid = (low + upp) // 2
     if(element>arr[mid]):
         low=mid+1
@@ -13,8 +14,9 @@ while(low<upp):
         upp=mid-1
     elif(element==arr[mid]):
         flag=1
+        pos=mid+1
         break
 if flag==0:
     print("element not found")
 else:
-    print("elemnt found")
+    print("elemnt found @",pos)
